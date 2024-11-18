e069b5c4b571c0806a06a3477ad02ce73851ac7c
QIMENGSHI <qms030313@outlook.com>
2024-11-18 19:58:26 +0200

    structure changed

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
-import routes from './routes/index';
+import routes from './src/index';
 
 const app = express();
 const PORT = 3000;
